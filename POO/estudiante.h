4bcab4a1cab2e8c8525cd4cdadf50163c436d2a0
Josue Téllez <josuetllezh@gmail.com>
2023-01-09 15:56:19 -0600

    Creacion de una clase en C++

---
+++ POO/estudiante.h
@@ -0,0 +1,96 @@
+/* 
+    Este archivo ".h" es una libreria de C/C++ aqui se implementa la creacion y definicion de la clase
+    que sera instanciada en un archivo ".cpp".
+*/
+#include <stdlib.h>
+using namespace std;
+
+class Estudiante
+{
+    //  Atributos de la clase.
+    private:
+        string matricula;
+        string nombre;
+        string apellido;
+        float promedio;
+
+    public:
+        //  Constructor de la clase.
+        Estudiante(string, string, string, float);
+
+        //  Declaracion de los metodos de la clase (getters y setters).
+        void set_matricula(string);
+        string get_matricula();
+        void set_nombre(string);
+        string get_nombre();
+        void set_apellido(string);
+        string get_apellido();
+        void set_promedio(float);
+        float get_promedio();
+        string to_string();
+
+};
+//  Inicializando constructor . . .
+Estudiante::Estudiante(string _matricula, string _nombre, string _apellido, float _promedio)
+{
+    matricula = _matricula;
+    nombre = _nombre;
+    apellido = _apellido;
+    promedio = _promedio;
+}
+
+//  Implementacion de los demas metodos.
+void Estudiante :: set_matricula(string _matricula)
+{
+    matricula = _matricula;
+}
+
+string Estudiante :: get_matricula()
+{
+    return matricula;
+}
+
+void Estudiante :: set_nombre(string _nombre)
+{
+    nombre = _nombre;
+}
+
+string Estudiante :: get_nombre()
+{
+    return nombre;
+}
+
+void Estudiante :: set_apellido(string _apellido)
+{
+    apellido = _apellido;
+}
+
+string Estudiante :: get_apellido()
+{
+    return apellido;
+}
+
+
+void Estudiante :: set_promedio(float _promedio)
+{
+    promedio = _promedio;
+}
+
+float Estudiante :: get_promedio()
+{
+    return promedio;
+}
+
+/*
+    No confundir el nombre de mi metodo to_string con el de la funcion para convertir a cualquier tipo de dato a 
+    cadena, no tenia conocimiento de la existencia de esa funcion. Conservo el nombre del metodo como convencion
+    de que asi se llama en el lengiaje Java.
+*/ 
+string Estudiante :: to_string()
+{
+    return "Matricula: " + matricula +
+            "\nNombre: "  + nombre +
+            "\nApellido: " + apellido +
+            "\nPromedio: " + std::to_string(promedio) +
+            "\n-------------------------";
+}
---
+++ POO/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "estudiante.h" //  Importando la libreria donde declare la clase.
+
+int main(int argc, char const *argv[])
+{
+    //  Instanciando la clase Estudiante
+    Estudiante *est1 = new Estudiante("S18001462", "Josue", "Tellez", 8.02);
+    
+    //  Obteniendo los datos que le pase al constructor de la clase iniciada.
+    string enrollment = est1->get_matricula();
+    string name = est1->get_nombre();
+    string lastname = est1->get_apellido();
+    float average = est1->get_promedio();
+
+    //  Imprimiendo las variables que obtuve.
+    cout << "Matricula: " << enrollment << endl;
+    cout << "Nombre: " << name << endl;
+    cout << "Apellido: " << lastname << endl;
+    cout << "Promedio: " << average << endl;
+    cout << "\n------------------------" << endl;
+
+    //  Actualizando promedio.
+    est1->set_promedio(8.2);
+
+    //  La funcion to_string devuelve una cadena con todo el contenido que estoy imprimiendo de la linea 16 a la 20.
+    cout << est1->to_string();
+
+    return 0;
+}
